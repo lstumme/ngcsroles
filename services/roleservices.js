5a324e5780e5269af9c9971d218809afee69b012
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-12-01 18:58:43 +0100

    Updating format

--- services/roleservices.js
+++ services/roleservices.js
@@ -8,7 +8,7 @@ const convertRole2Object = r => {
 		subRoles: r.subRoles.map(el => { return el.toString() }),
 	};
 };
- 
+
 exports.createRole = async ({ name, label }) => {
 	const role = new Role({
 		name: name,
@@ -39,23 +39,22 @@ exports.deleteRole = async ({ roleId }) => {
 };
 
 exports.getRoles = async ({ page, perPage }) => {
-    return Role.countDocuments()
-        .then(count => {
-            const pageCount = Math.trunc(count / perPage) + (count % perPage > 0 ? 1 : 0);
-            if (count <= perPage * (page - 1) || (perPage * (page - 1) < 0)) {
-                const error = new Error('Pagination out of bounds.');
-                error.statusCode = 400;
-                throw error;
-            }
-            return Role.find().skip((page - 1) * perPage).limit(Number.parseInt(perPage))
-                .then(result => {
-                    return {
-                        roles: result.map(r => { return convertRole2Object(r); }),
-                        pageCount: pageCount
-                    };
-                })
-        });
-
+	return Role.countDocuments()
+		.then(count => {
+			const pageCount = Math.trunc(count / perPage) + (count % perPage > 0 ? 1 : 0);
+			if (count <= perPage * (page - 1) || (perPage * (page - 1) < 0)) {
+				const error = new Error('Pagination out of bounds.');
+				error.statusCode = 400;
+				throw error;
+			}
+			return Role.find().skip((page - 1) * perPage).limit(Number.parseInt(perPage))
+				.then(result => {
+					return {
+						roles: result.map(r => { return convertRole2Object(r); }),
+						pageCount: pageCount
+					};
+				})
+		});
 };
 
 exports.getRole = async ({ roleId }) => {
@@ -88,7 +87,7 @@ exports.addSubRoleToRole = async ({ roleId, subRoleId }) => {
 						error.statusCode = 404;
 						throw error;
 					}
-					const roleObject = convertRole2Object(role); 
+					const roleObject = convertRole2Object(role);
 					const index = roleObject.subRoles.indexOf(subRoleId);
 					if (index >= 0) {
 						const error = new Error('Role already in subRoles');
@@ -122,7 +121,7 @@ exports.removeSubRoleFromRole = async ({ roleId, subRoleId }) => {
 						error.statusCode = 404;
 						throw error;
 					}
-					const roleObject = convertRole2Object(role); 
+					const roleObject = convertRole2Object(role);
 					const index = roleObject.subRoles.indexOf(subRoleId);
 					if (index < 0) {
 						const error = new Error('Role not in subRoles');
@@ -143,9 +142,9 @@ exports.findRoleByName = async ({ name }) => {
 	return Role.findOne({ name: name })
 		.then(role => {
 			if (!role) {
-	            const error = new Error('Could not find Role');
-	            error.statusCode = 404;
-	            throw error;
+				const error = new Error('Could not find Role');
+				error.statusCode = 404;
+				throw error;
 			}
 			return convertRole2Object(role);
 		});
@@ -155,9 +154,9 @@ exports.findRoleByLabel = async ({ label }) => {
 	return Role.findOne({ label: label })
 		.then(role => {
 			if (!role) {
-	            const error = new Error('Could not find Role');
-	            error.statusCode = 404;
-	            throw error;
+				const error = new Error('Could not find Role');
+				error.statusCode = 404;
+				throw error;
 			}
 			return convertRole2Object(role);
 		});
